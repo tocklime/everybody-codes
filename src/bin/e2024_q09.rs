0a414dc5ffed8d56b79125f67c39c8727fa8d371
Greg Manning <greg@gregmanning.uk>
2024-11-18 18:42:58 +0000

    tidied

--- src/bin/e2024_q09.rs
+++ src/bin/e2024_q09.rs
@@ -1,12 +1,9 @@
-use std::collections::HashMap;
-
 use everybody_codes::collections::VecLookup;
 
 const P1_INPUT: &str = include_str!("../../inputs/everybody_codes_e2024_q09_p1.txt");
 const P2_INPUT: &str = include_str!("../../inputs/everybody_codes_e2024_q09_p2.txt");
 const P3_INPUT: &str = include_str!("../../inputs/everybody_codes_e2024_q09_p3.txt");
 
-const STAMPS: [u32; 4] = [10, 5, 3, 1];
 fn main() {
     println!("P1: {}", solve::<1>(P1_INPUT));
     println!("P2: {}", solve::<2>(P2_INPUT));
@@ -14,35 +11,17 @@ fn main() {
 }
 fn solve<const PART: usize>(input: &str) -> u32 {
     let ns: Vec<u32> = input.lines().map(|x| x.parse().unwrap()).collect();
+    let mut dp = match PART {
+        1 => Dp::new(&[1, 3, 5, 10]),
+        2 => Dp::new(&[1, 3, 5, 10, 15, 16, 20, 24, 25, 30]),
+        3 => Dp::new(&[
+            1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101,
+        ]),
+        _ => unreachable!(),
+    };
     match PART {
-        1 => ns
-            .iter()
-            .map(|n| {
-                let mut n = *n;
-                let mut count = 0;
-                for s in STAMPS {
-                    count += n / s;
-                    n = n % s;
-                }
-                assert_eq!(n, 0);
-                count
-            })
-            .sum(),
-        2 => ns
-            .iter()
-            .map(|&n| change_dp(n, &[1, 3, 5, 10, 15, 16, 20, 24, 25, 30]))
-            .sum(),
-        3 => ns
-            .iter()
-            .map(|&n| {
-                p3(
-                    n,
-                    &[
-                        1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101,
-                    ],
-                )
-            })
-            .sum(),
+        1 | 2 => ns.iter().map(|&n| dp.get(n as usize)).sum(),
+        3 => ns.iter().map(|&n| dp.split_and_search(n as usize)).sum(),
         _ => unimplemented!(),
     }
 }
@@ -56,15 +35,20 @@ impl Dp {
     fn new(stamps: &[u32]) -> Self {
         let mut data = VecLookup::with_capacity(1000);
         data.insert(0, 0);
-        Self { data, stamps: stamps.to_vec(), correct_up_to: 0 }
+        Self {
+            data,
+            stamps: stamps.to_vec(),
+            correct_up_to: 0,
+        }
     }
     fn make_correct_to(&mut self, target: usize) {
         for n in self.correct_up_to..=target {
             let stamp_count = self.data[n] + 1;
             for &s in &self.stamps {
-                let x= self.data.entry(n+(s as usize))
-                    // .and_modify(|x| *x = (*x).min(stamp_count))
-                    .or_insert_with(||stamp_count);
+                let x = self
+                    .data
+                    .entry(n + (s as usize))
+                    .or_insert_with(|| stamp_count);
                 if *x > stamp_count {
                     *x = stamp_count;
                 }
@@ -72,38 +56,26 @@ impl Dp {
         }
         self.correct_up_to = target;
     }
-}
-//dp is map of best way to get N beetles stamped.
-fn change_dp(target: u32, stamps: &[u32]) -> u32 {
-    let mut dp: HashMap<u32, u32> = HashMap::new();
-    dp.insert(0, 0);
-    for n in 0..=target {
-        let stamp_count = dp[&n] + 1;
-        for &s in stamps {
-            dp.entry(n + s)
-                .and_modify(|x| *x = (*x).min(stamp_count))
-                .or_insert(stamp_count);
-        }
+    fn get(&mut self, target: usize) -> u32 {
+        self.make_correct_to(target);
+        self.data[target]
     }
-    dp[&target]
-}
-fn p3(target: u32, stamps: &[u32]) -> u32 {
-    let mut dp = Dp::new(stamps);
-    let a_1 = target / 2 - 50;
+    fn split_and_search(&mut self, target: usize) -> u32 {
+        let a_1 = target / 2 - 50;
 
-    (0..=101)
-        .map(|add| {
-            let a = a_1 + add;
-            let b = target - a;
-            if a.abs_diff(b) <=100 {
-                dp.make_correct_to(b as usize);
-                dp.data[a as usize] + dp.data[b as usize]
-            } else {
-                u32::MAX
-            }
-        })
-        .min()
-        .unwrap()
+        (0..=101)
+            .map(|add| {
+                let a = a_1 + add;
+                let b = target - a;
+                if a.abs_diff(b) <= 100 {
+                    self.get(a as usize) + self.get(b as usize)
+                } else {
+                    u32::MAX
+                }
+            })
+            .min()
+            .unwrap()
+    }
 }
 
 #[cfg(test)]
