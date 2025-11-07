f639dc1ce09dd56febd92c0578aa1503eb39ae27
Greg Manning <greg@gregmanning.uk>
2025-11-07 23:51:23 +0000

    e2025q5 tidied up

--- src/bin/e2025_q05.rs
+++ src/bin/e2025_q05.rs
@@ -1,6 +1,12 @@
 use std::cmp::Reverse;
 
+use everybody_codes::nums::digits;
 use itertools::Itertools;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, newline},
+    Parser,
+};
 
 const P1_INPUT: &str = include_str!("../../inputs/everybody_codes_e2025_q05_p1.txt");
 const P2_INPUT: &str = include_str!("../../inputs/everybody_codes_e2025_q05_p2.txt");
@@ -13,35 +19,41 @@ fn main() {
 }
 
 fn concat_digits<T: IntoIterator<Item = u64>>(t: T) -> u64 {
-    t.into_iter().map(|x| x.to_string()).join("").parse().unwrap()
+    t.into_iter()
+        .flat_map(digits)
+        .fold(0, |acc, d| 10 * acc + d)
 }
 
 struct Segment {
     left: Option<u64>,
     spine: u64,
-    right: Option<u64>
+    right: Option<u64>,
 }
 impl Segment {
     fn new(spine: u64) -> Self {
         Self {
-            spine, left: None, right: None
+            spine,
+            left: None,
+            right: None,
         }
     }
     fn insert(&mut self, n: u64) -> bool {
         if n < self.spine && self.left.is_none() {
             self.left = Some(n);
             true
-        }
-        else if n > self.spine && self.right.is_none() {
+        } else if n > self.spine && self.right.is_none() {
             self.right = Some(n);
             true
-        }
-        else {
+        } else {
             false
         }
     }
     fn level_number(&self) -> u64 {
-        concat_digits([self.left, Some(self.spine), self.right].iter().filter_map(|x| *x))
+        concat_digits(
+            [self.left, Some(self.spine), self.right]
+                .iter()
+                .filter_map(|x| *x),
+        )
     }
 }
 struct Sword {
@@ -49,64 +61,76 @@ struct Sword {
     segments: Vec<Segment>,
 }
 impl Sword {
-    fn parse(input: &str) -> Self {
-        let (id, ns) = input.split_once(':').unwrap();
-        let ns : Vec<u64> = ns.split(',').map(|x| str::parse::<u64>(x).unwrap()).collect();
-        let mut segments : Vec<Segment> = Vec::new();
-        for n in ns {
-            let mut found = false;
-            for segment in &mut segments {
-                if segment.insert(n) {
-                    found = true;
-                    break;
-                }
-            }
-            if !found {
+    fn parser<'a>() -> impl Parser<&'a str, Output = Self, Error = nom::error::Error<&'a str>> {
+        (
+            complete::u64,
+            tag(":"),
+            nom::multi::separated_list1(tag(","), complete::u64),
+        )
+            .map(|(id, _, ns)| Self::new(id, &ns))
+    }
+    fn new(id: u64, ns: &[u64]) -> Self {
+        let mut segments: Vec<Segment> = Vec::new();
+        for &n in ns {
+            if segments.iter_mut().position(|x| x.insert(n)).is_none() {
                 segments.push(Segment::new(n));
             }
         }
-        Self { id: id.parse().unwrap(), segments}
+        Self { id, segments }
     }
     fn make_cmp_key(&self) -> (u64, Vec<u64>, u64) {
-        (self.quality(), self.segments.iter().map(|x| x.level_number()).collect(), self.id)
+        (
+            self.quality(),
+            self.segments.iter().map(|x| x.level_number()).collect(),
+            self.id,
+        )
     }
     fn quality(&self) -> u64 {
         concat_digits(self.segments.iter().map(|x| x.spine))
     }
 }
 
+fn parse(input: &str) -> Vec<Sword> {
+    nom::multi::separated_list1(newline, Sword::parser())
+        .parse(input)
+        .unwrap()
+        .1
+}
+
 fn assess_sword(input: &str) -> u64 {
-    Sword::parse(input).quality()
+    parse(input)[0].quality()
 }
 fn get_checksum(input: &str) -> u64 {
-    let mut swords : Vec<Sword> = input.lines().map(Sword::parse).collect();
+    let mut swords: Vec<Sword> = parse(input);
     swords.sort_by_cached_key(|x| Reverse(x.make_cmp_key()));
-    let ids = swords.iter().map(|x| x.id).collect_vec();
-    ids.iter().zip(1..).map(|(id, ix)| id * ix).sum()
-
+    swords
+        .iter()
+        .map(|x| x.id)
+        .zip(1..)
+        .map(|(id, ix)| id * ix)
+        .sum()
 }
 
 fn find_quality_range(input: &str) -> u64 {
-    let x = input.lines().map(assess_sword).minmax();
+    let x = parse(input).iter().map(Sword::quality).minmax();
     match x {
         itertools::MinMaxResult::NoElements => panic!(),
         itertools::MinMaxResult::OneElement(_) => 0,
-        itertools::MinMaxResult::MinMax(a, b) => a.abs_diff(b)
+        itertools::MinMaxResult::MinMax(a, b) => a.abs_diff(b),
     }
 }
-
 
 #[cfg(test)]
 mod test {
     use super::*;
-    const EG1 : &str = "58:5,3,7,8,9,10,4,5,7,8,8";
+    const EG1: &str = "58:5,3,7,8,9,10,4,5,7,8,8";
     #[test]
     fn p1_example() {
         assert_eq!(assess_sword(EG1), 581078);
     }
     #[test]
     fn p2_example() {
-        const EG : &str = "1:2,4,1,1,8,2,7,9,8,6
+        const EG: &str = "1:2,4,1,1,8,2,7,9,8,6
 2:7,9,9,3,8,3,8,8,6,8
 3:4,7,6,9,1,8,3,7,2,2
 4:6,4,2,1,7,4,5,5,5,8
@@ -120,7 +144,7 @@ mod test {
     }
     #[test]
     fn p3_example() {
-        const EG : &str = "1:7,1,9,1,6,9,8,3,7,2
+        const EG: &str = "1:7,1,9,1,6,9,8,3,7,2
 2:6,1,9,2,9,8,8,4,3,1
 3:7,1,9,1,6,9,8,3,8,3
 4:6,1,9,2,8,8,8,4,3,1
