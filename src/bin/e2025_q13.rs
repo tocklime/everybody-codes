2dc5871637a711a382ec70d340f50101ea2a2dfe
Greg Manning <greg@gregmanning.uk>
2025-11-19 23:47:16 +0000

    e2025_q13 tidied up some

--- src/bin/e2025_q13.rs
+++ src/bin/e2025_q13.rs
@@ -1,3 +1,8 @@
+use itertools::{
+    FoldWhile::{Continue, Done},
+    Itertools,
+};
+
 const P1_INPUT: &str = include_str!("../../inputs/everybody_codes_e2025_q13_p1.txt");
 const P2_INPUT: &str = include_str!("../../inputs/everybody_codes_e2025_q13_p2.txt");
 const P3_INPUT: &str = include_str!("../../inputs/everybody_codes_e2025_q13_p3.txt");
@@ -7,62 +12,47 @@ fn main() {
     println!("P2: {}", solve::<2>(P2_INPUT));
     println!("P3: {}", solve::<3>(P3_INPUT));
 }
-fn solve<const PART: usize>(input: &str) -> usize {
-    match PART {
-        1 => {
-            let ns: Vec<usize> = input.lines().map(|x| x.parse().unwrap()).collect();
-            let mut f = Vec::new();
-            let mut b = Vec::new();
-            f.push(1);
-            for (ix, n) in ns.iter().enumerate() {
-                if ix % 2 == 0 {
-                    f.push(*n);
-                } else {
-                    b.push(*n);
-                }
-            }
-            f.extend(b.iter().rev());
-            f[2025 % f.len()]
-        }
-        2 | 3 => {
-            let ns : Vec<(usize,usize)> = input.lines().map(|l| {
-                let (a,b) = l.split_once('-').unwrap();
+fn solve<const PART: usize>(input: &str) -> i64 {
+    let ns: Vec<(i64, i64)> = input
+        .lines()
+        .map(|l| {
+            if let Some((a, b)) = l.split_once('-') {
                 (a.parse().unwrap(), b.parse().unwrap())
-            }).collect();
-            let total = 1 + ns.iter().map(|(a,b)| 1 + b - a).sum::<usize>();
-            let count = if PART == 2 { 20252025} else { 202520252025};
-            let ix = count % total;
-            // dbg!(total, ix);
-            let mut f = Vec::new();
-            let mut ba = Vec::new();
-            f.push((1,1));
-            for (ix, (a,b)) in ns.iter().enumerate() {
-                if ix % 2 == 0 {
-                    f.push((*a,*b));
-                } else {
-                    ba.push((*b,*a));
-                }
+            } else {
+                let a = l.parse().unwrap();
+                (a, a)
             }
-            f.extend(ba.iter().rev());
-            let mut remaining = ix;
-            for (a,b) in &f {
-                let count_here = b.abs_diff(*a) + 1;
-                // dbg!(a,b,count_here, remaining);
-                if remaining >= count_here {
-                    remaining -= count_here;
-                } else {
-                    if a>b {
-                        //going backward from a to b.
-                        return a - remaining
-                    } else {
-                        return a + remaining
-                    }
-                }
-            }
-            todo!()
-        }
-        _ => unimplemented!(),
+        })
+        .collect();
+    let total = 1 + ns.iter().map(|(a, b)| 1 + b - a).sum::<i64>();
+    let count = match PART {
+        1 => 2025,
+        2 => 20252025,
+        3 => 202520252025,
+        _ => unreachable!(),
+    };
+    let ix = count % total;
+    let mut f = Vec::new();
+    f.push((1, 1, 1));
+    //add all the even indexes.
+    for ix in (0..ns.len()).step_by(2) {
+        let (a, b) = ns[ix];
+        f.push((a, 1, b - a + 1))
     }
+    //add all the odd indexes, backward.
+    for ix in (0..ns.len()).rev().step_by(2) {
+        let (a, b) = ns[ix];
+        f.push((b, -1, b - a + 1));
+    }
+    f.into_iter()
+        .fold_while(ix, |rem, (start, delta, count)| {
+            if rem >= count {
+                Continue(rem - count)
+            } else {
+                Done(start + (rem * delta))
+            }
+        })
+        .into_inner()
 }
 
 #[cfg(test)]
@@ -86,15 +76,11 @@ mod test {
 30-37";
         assert_eq!(solve::<2>(EG2), 30);
     }
-    // #[test]
-    // fn p3_example() {
-    //     assert_eq!(solve::<3>(EG3), 0);
-    // }
 
-    // #[test]
-    // fn correct_answers() {
-    //     assert_eq!(solve::<1>(P1_INPUT), 0);
-    //     assert_eq!(solve::<2>(P2_INPUT), 0);
-    //     assert_eq!(solve::<3>(P3_INPUT), 0);
-    // }
+    #[test]
+    fn correct_answers() {
+        assert_eq!(solve::<1>(P1_INPUT), 212);
+        assert_eq!(solve::<2>(P2_INPUT), 3500);
+        assert_eq!(solve::<3>(P3_INPUT), 170354);
+    }
 }
