c4f5ebdb27b57a9200201699fe31f73ea468a0fa
Greg Manning <greg@gregmanning.uk>
2026-03-03 23:57:42 +0000

    e3q1 tidied up

--- src/bin/e3_q01.rs
+++ src/bin/e3_q01.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::{cmp::Reverse, collections::BTreeMap};
 
 use itertools::Itertools;
 
@@ -7,74 +7,99 @@ const P2_INPUT: &str = include_str!("../../inputs/everybody_codes_e3_q01_p2.txt"
 const P3_INPUT: &str = include_str!("../../inputs/everybody_codes_e3_q01_p3.txt");
 
 fn main() {
-    println!("P1: {}", solve::<1>(P1_INPUT));
-    println!("P2: {}", solve::<2>(P2_INPUT));
-    println!("P3: {}", solve::<3>(P3_INPUT));
+    println!("P1: {}", p1(P1_INPUT));
+    println!("P2: {}", p2(P2_INPUT));
+    println!("P3: {}", p3(P3_INPUT));
 }
-fn solve<const PART: usize>(input: &str) -> usize {
-    let mut p2_vals = Vec::new();
-    let mut p3_vals : BTreeMap<(usize, bool), Vec<usize>> = BTreeMap::new();
-    let mut ans = 0;
-    for l in input.lines() {
-        let (id, rest) = l.split_once(':').unwrap();
-        let id = usize::from_str_radix(id, 10).unwrap();
-        let words = rest.split(' ').collect::<Vec<&str>>();
-        let vals = words.into_iter().map(|w| w.chars().rev().enumerate().map(|(ix, c)| if c.is_uppercase() {
-            1<<ix
-        } else { 0}).sum::<u32>()).collect::<Vec<u32>>();
-        let max = vals[0..3].iter().max().unwrap();
-        let max_count = vals[0..3].iter().filter(|x| x == &max).count();
-        let max_pos = vals[0..3].iter().position_max().unwrap();
-        match PART {
-            1 => {
-                if max_count == 1 && max_pos == 1 {
-                    ans += id;
-                }
-            }
-            2 => {
-                p2_vals.push((id, vals));
-            }
-            3 => {
-                if !(31..33).contains(&vals[3]) && max_count == 1{
-                    let is_shiny = vals[3] >= 33;
-                    p3_vals.entry((max_pos, is_shiny)).or_default().push(id);
-                }
-            }
-            _ => unimplemented!()
+struct Scale {
+    id: usize,
+    cols: [u32; 3],
+    shininess: u32,
+}
+impl Scale {
+    fn main_col_ix(&self) -> Option<usize> {
+        let max_val = self.cols.iter().max().unwrap();
+        let max_count = self.cols.iter().filter(|x| x == &max_val).count();
+        (max_count == 1).then_some(self.cols.iter().position_max().unwrap())
+    }
+    fn brightness(&self) -> u32 {
+        self.cols.iter().sum()
+    }
+    fn is_shiny(&self) -> Option<bool> {
+        match self.shininess {
+            0..=30 => Some(false),
+            33.. => Some(true),
+            _ => None,
         }
     }
-    if PART == 2 {
-        let best_shine = p2_vals.iter().max_by_key(|x| (x.1[3], u32::MAX-(x.1[0..2].iter().sum::<u32>()))).unwrap();
-        return best_shine.0;
-    }
-    if PART == 3 {
-        let biggest = p3_vals.iter().max_by_key(|x| x.1.len()).unwrap();
-        return biggest.1.iter().sum::<usize>();
-    }
-    ans
 }
-
+fn generate(input: &str) -> Vec<Scale> {
+    input
+        .lines()
+        .map(|l| {
+            let (id, rest) = l.split_once(':').unwrap();
+            let id = str::parse(id).unwrap();
+            let words = rest.split(' ');
+            let vals = words
+                .map(|w| {
+                    w.chars()
+                        .rev()
+                        .enumerate()
+                        .map(|(ix, c)| if c.is_uppercase() { 1 << ix } else { 0 })
+                        .sum::<u32>()
+                })
+                .collect::<Vec<u32>>();
+            let (cols, rest) = vals.split_at(3);
+            Scale {
+                id,
+                cols: cols.try_into().unwrap(),
+                shininess: rest.first().copied().unwrap_or_default(),
+            }
+        })
+        .collect()
+}
+fn p1(input: &str) -> usize {
+    generate(input)
+        .into_iter()
+        .map(|s| s.main_col_ix().and_then(|x| (x == 1).then_some(s.id)).unwrap_or_default())
+        .sum()
+}
+fn p2(input: &str) -> usize {
+    generate(input)
+        .iter()
+        .max_by_key(|x| (x.shininess, Reverse(x.brightness())))
+        .unwrap().id
+}
+fn p3(input: &str) -> usize {
+    let mut grouped: BTreeMap<(usize, bool), Vec<usize>> = BTreeMap::new();
+    for s in generate(input) {
+        if let (Some(ix), Some(shiny)) = (s.main_col_ix(), s.is_shiny()) {
+            grouped.entry((ix, shiny)).or_default().push(s.id);
+        }
+    }
+    grouped.into_values().max_by_key(Vec::len).unwrap().iter().sum()
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
-    const EG1 : &str = "2456:rrrrrr ggGgGG bbbbBB
+    const EG1: &str = "2456:rrrrrr ggGgGG bbbbBB
 7689:rrRrrr ggGggg bbbBBB
 3145:rrRrRr gggGgg bbbbBB
 6710:rrrRRr ggGGGg bbBBbB";
     #[test]
     fn p1_example() {
-        assert_eq!(solve::<1>(EG1), 9166);
+        assert_eq!(p1(EG1), 9166);
     }
-    const EG2 : &str = "2456:rrrrrr ggGgGG bbbbBB sSsSsS
+    const EG2: &str = "2456:rrrrrr ggGgGG bbbbBB sSsSsS
 7689:rrRrrr ggGggg bbbBBB ssSSss
 3145:rrRrRr gggGgg bbbbBB sSsSsS
 6710:rrrRRr ggGGGg bbBBbB ssSSss";
     #[test]
     fn p2_example() {
-        assert_eq!(solve::<2>(EG2), 2456);
+        assert_eq!(p2(EG2), 2456);
     }
-    const EG3 : &str = "15437:rRrrRR gGGGGG BBBBBB sSSSSS
+    const EG3: &str = "15437:rRrrRR gGGGGG BBBBBB sSSSSS
 94682:RrRrrR gGGggG bBBBBB ssSSSs
 56513:RRRrrr ggGGgG bbbBbb ssSsSS
 76346:rRRrrR GGgggg bbbBBB ssssSs
@@ -92,13 +117,13 @@ mod test {
 80219:rRRrRR GGGggg BBbbbb SssSSs";
     #[test]
     fn p3_example() {
-        assert_eq!(solve::<3>(EG3), 292320);
+        assert_eq!(p3(EG3), 292320);
     }
 
-    // #[test]
-    // fn correct_answers() {
-    //     assert_eq!(solve::<1>(P1_INPUT), 0);
-    //     assert_eq!(solve::<2>(P2_INPUT), 0);
-    //     assert_eq!(solve::<3>(P3_INPUT), 0);
-    // }
+    #[test]
+    fn correct_answers() {
+        assert_eq!(p1(P1_INPUT), 61110);
+        assert_eq!(p2(P2_INPUT), 17039);
+        assert_eq!(p3(P3_INPUT), 10578316);
+    }
 }
